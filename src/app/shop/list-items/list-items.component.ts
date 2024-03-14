b5b714a99926d55f953636ee92bf2ded699ea4b2
ThomasKvam <thomas.e.h.kvam@hotmail.no>
2024-03-14 14:34:29 +0100

    Able to sort by name and price now. Not at the same time. All resets everything

--- src/app/shop/list-items/list-items.component.ts
+++ src/app/shop/list-items/list-items.component.ts
@@ -9,6 +9,7 @@ import { ItemService } from 'src/app/services/item.service';
 })
 export class ListItemsComponent {
   items: Item[] = []
+  originalOrder: Item[] = []
   vegetables: Item[] = []
   fruits: Item[] = []
   showVegetables: boolean = false
@@ -22,18 +23,19 @@ export class ListItemsComponent {
 
  async renderStore(){
     this.items = await this.itemService.getItems()
+    this.originalOrder = await this.itemService.getItems()
+    console.log(this.originalOrder)
+
     this.fruits =[]
     this.vegetables= []
 
     for (let item of this.items) {
       if (item.type === 'vegetable') {
         this.vegetables.push(item);
-        console.log(this.vegetables);
       } else if (item.type === 'fruit') { 
         this.fruits.push(item);
-        console.log(this.fruits);
+      }
     }
-  }
   }
 
   addItemToCart(item: Item){
@@ -50,8 +52,23 @@ export class ListItemsComponent {
     this.showVegetables = false
   }
   showAll(){
+    this.items = this.originalOrder
+    console.log(this.items)
     this.showFruits = false
     this.showVegetables = false
+    
+  }
+
+  sortByPrice(){
+    const itemsCopy = [...this.originalOrder]
+    itemsCopy.sort((a,b) => a.price - b.price)
+    this.items = itemsCopy
+  }
+
+  sortByName(){
+    const itemsCopy = [...this.originalOrder]
+    itemsCopy.sort((a,b) => a.name.localeCompare(b.name))
+    this.items = itemsCopy
   }
 
 }
